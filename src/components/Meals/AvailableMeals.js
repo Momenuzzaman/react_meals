56e96cbbd023aaea18553b75e21c0b222b2da437
mdemon420 <mdeamon4@gmail.com>
2023-01-22 13:06:30 +0600

    available meal

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
+const DUMMY_MEALS = [
+    {
+        id: 'm1',
+        name: 'Sushi',
+        description: 'Finest fish and veggies',
+        price: 22.99,
+    },
+    {
+        id: 'm2',
+        name: 'Schnitzel',
+        description: 'A german specialty!',
+        price: 16.5,
+    },
+    {
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 12.99,
+    },
+    {
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 18.99,
+    },
+];
 const AvailableMeals = () => {
+    const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
     return (
-        <div>
-
-        </div>
+        <section>
+            <ul>
+                {mealList}
+            </ul>
+        </section>
     );
 };
 
