404544275222910fa18b063bebab63745504c881
mdemon420 <mdeamon4@gmail.com>
2023-01-23 00:26:12 +0600

    props pass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
   return (
     <div>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
