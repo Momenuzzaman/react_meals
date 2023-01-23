0a97350d3c9417757e22f959bc453f02b4b3324d
mdemon420 <mdeamon4@gmail.com>
2023-01-23 18:29:16 +0600

    remove item

--- src/store/CartProviders.js
+++ src/store/CartProviders.js
@@ -9,7 +9,6 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        // const updatedItems = state.items.concat(action.item)
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
 
         const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id)
@@ -27,6 +26,24 @@ const cartReducer = (state, action) => {
         }
         else {
             updatedItems = state.items.concat(action.item);
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id)
+        }
+        else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem
         }
         return {
             items: updatedItems,
