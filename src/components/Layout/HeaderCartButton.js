6dac48d854ff4b4e6e66dacad54cdbaa97a2a561
mdemon420 <mdeamon4@gmail.com>
2023-01-27 16:39:31 +0600

    fetch data and load

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
     const cartCtx = useContext(CartContext);
-
-    const numbersOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    const { items } = cartCtx;
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const numbersOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount;
     }, 0);
+
+
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+
 const AvailableMeals = () => {
-    const mealList = DUMMY_MEALS.map(meal => <MealItem key={meal.id}
+    const [meals, setMeals] = useState([]);
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-https-12f16-default-rtdb.firebaseio.com/meals.json');
+            const responseData = await response.json();
+
+            const loadMeals = [];
+            for (let key in responseData) {
+                loadMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    price: responseData[key].price,
+                    description: responseData[key].description
+                });
+            }
+            setMeals(loadMeals);
+        };
+        fetchMeals();
+    }, []);
+
+    const mealList = meals.map(meal => <MealItem key={meal.id}
         id={meal.id}
         name={meal.name}
         price={meal.price}
