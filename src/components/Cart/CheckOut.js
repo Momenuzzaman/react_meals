f5c2e1a971a97b0d91d607b0fd54caab65ce453c
mdemon420 <mdeamon4@gmail.com>
2023-01-28 16:24:22 +0600

    form style add

--- src/components/Cart/CheckOut.js
+++ src/components/Cart/CheckOut.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import classes from './CartItem.module.css';
+import classes from './CheckOut.module.css';
+
 const CheckOut = (props) => {
     const confirmHandler = (event) => {
         event.preventDefault();
     };
     return (
-        <form onSubmit={confirmHandler}>
+        <form className={classes.form} onSubmit={confirmHandler}>
             <div className={classes.control}>
                 <label htmlFor="name">Your Name</label>
                 <input type="text" id="name" />
@@ -22,10 +23,11 @@ const CheckOut = (props) => {
                 <label htmlFor="city">City</label>
                 <input type="text" id="city" />
             </div>
-            <button type='button' onClick={props.onCancel}>Cancel</button>
-            <button>Confirm</button>
+            <div className={classes.actions}>
+                <button type='button' onClick={props.onCancel}>Cancel</button>
+                <button className={classes.submit}>Confirm</button>
+            </div>
         </form>
     );
 };
-
 export default CheckOut;
